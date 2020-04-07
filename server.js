395dc87f3fbb658ceec632a95708c74f7c4465dd
bramos33 <bramosd@yahoo.com>
2020-04-07 16:03:08 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-var loggedIn = false
 
 app.set('views', './views')
 app.set('view engine', 'ejs')
